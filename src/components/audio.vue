8a3ecb92543ad0d47ae197df7e4c166aa766fc68
“forheyin <forheyin@163.com>
2018-03-20 17:41:50 +0800

    可拖动进度条

--- src/components/audio.vue
+++ src/components/audio.vue
@@ -2,17 +2,18 @@
     <div class="audio">
       <div class="con">
         <span class="iconfont icon-last"></span>
-        <!--<span :class="[isPlay?'icon-zanting':'icon-bo','iconfont']"></span>-->
         <span class="iconfont icon-zanting" @click="stop" v-if="isPlay"></span>
         <span class="iconfont icon-play" @click="play" v-else ></span>
         <span class="iconfont icon-next"></span>
       </div>
       <div class="pro">
         <span>{{currentTime | timeFormat}}</span>
-        <i>
-          <b :style="{left: step + '%'}" id="steps"></b>
-          <em :style="{width: step + '%'}"></em>
-        </i>
+        <p @mousemove="mousemove($event)" @mouseleave="mouseEnd($event)" id="pro">
+          <i @click="jumpTime($event)">
+            <b :style="{left: step + '%'}" id="steps" @mousedown="mousedown($event)" @mouseup="mouseEnd($event)"></b>
+            <em :style="{width: step + '%'}"></em>
+          </i>
+        </p>
         <span>{{$store.state.duration | timeFormat}}</span>
       </div>
       <audio class="play" id="playSong" @timeupdate="updateLyric">
@@ -28,7 +29,10 @@ export default {
       mp3Url: '',
       isPlay: false,
       step: 0,
-      currentTime: 0
+      currentTime: 0,
+      locked: false,
+      lastStep: 0,
+      time: 0
     }
   },
   computed: {
@@ -52,10 +56,11 @@ export default {
   mounted () {
   },
   methods: {
-    updateLyric (event) {
+    updateLyric () {
       let playSong = document.getElementById('playSong')
+      let t = playSong.currentTime
       if (this.$route.path === '/musicPlay') {
-        Bus.$emit('getTarget', playSong.currentTime)
+        Bus.$emit('getTarget', t)
       }
       this.currentTime = playSong.currentTime * 1000
       if (this.step <= 100) {
@@ -71,6 +76,28 @@ export default {
       let playSong = document.getElementById('playSong')
       playSong.pause()
       this.isPlay = false
+    },
+    mousedown (e) {
+      this.locked = true
+      this.lastStep = this.step
+    },
+    mouseEnd (e) {
+      this.locked = false
+    },
+    mousemove (e) {
+      if (this.locked) {
+        this.same(e)
+      }
+    },
+    jumpTime (e) {
+      this.same(e)
+    },
+    same (e) {
+      // 点击或移动时根据step计算time赋值给playSong.currentTime
+      this.step = e.offsetX / 445 * 100
+      this.time = this.step * this.$store.state.duration / 1000 / 100
+      let playSong = document.getElementById('playSong')
+      playSong.currentTime = this.time
     }
   }
 }
@@ -101,42 +128,49 @@ export default {
       width: 535px;
       flex-shrink: 0;
       display: flex;
-      align-items: center;
-      i {
-        width: 442.5px;
-        flex-shrink: 0;
-        height: 4px;
-        background: #C2C2C4;
-        border-radius: 3px;
-        position: relative;
-        em {
-          position: absolute;
-          width: 0;
+      height: 50px;
+      line-height: 50px;
+      p {
+        flex: 1;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        i {
+          width: 445px;
+          flex-shrink: 0;
           height: 4px;
-          background: #c62f2f;
-          z-index: 86;
-          border-radius: 3px;
-        }
-        b {
-          position: absolute;
-          left: 0;
-          top: -5px;
-          width: 14px;
-          height: 14px;
-          background: #fff;
-          border-radius: 50%;
-          border: 0.5px solid #ddd;
-          cursor: pointer;
-          z-index: 88;
-          &:after {
+          background: #C2C2C4;
+          border-radius: 5px;
+          position: relative;
+          em {
             position: absolute;
-            content: '';
-            width: 4px;
+            width: 0;
             height: 4px;
             background: #c62f2f;
+            z-index: 86;
+            border-radius: 5px;
+          }
+          b {
+            position: absolute;
+            left: 0;
+            top: -5px;
+            width: 14px;
+            height: 14px;
+            background: #fff;
             border-radius: 50%;
-            top: 4.5px;
-            left: 4.5px;
+            border: 0.5px solid #ddd;
+            cursor: pointer;
+            z-index: 88;
+            &:after {
+              position: absolute;
+              content: '';
+              width: 4px;
+              height: 4px;
+              background: #c62f2f;
+              border-radius: 50%;
+              top: 4.5px;
+              left: 4.5px;
+            }
           }
         }
       }
